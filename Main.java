d3e079af99937311adc5ba2c86d390c50de02947
enriquesales <enricsales@hotmail.com>
2025-10-25 18:05:05 +0000

    Initial commit: java project

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * auto-repo-mh6jp3qn-2 - Java Implementation
+ * A simple Hello World program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to auto-repo-mh6jp3qn-2");
+        
+        // Simple calculation
+        int[] numbers = {1, 2, 3, 4, 5};
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        System.out.println("Sum of numbers: " + total);
+    }
+}
